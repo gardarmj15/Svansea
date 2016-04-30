cb63230eaff2bc8b7e78aa0e514c47481e893846
gardarmj15 <gardarmj15@ru.is>
2016-04-30 12:13:44 +0000

    adf

---
+++ testtt/testtt/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(testtt.Startup))]
+namespace testtt
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
