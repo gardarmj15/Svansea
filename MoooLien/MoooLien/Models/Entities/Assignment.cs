340ae05df9eea025928c9063151e6d2e66db2af5
e-magnus <einare15@ru.is>
2016-05-06 15:04:32 +0000

    Assingments virkar!

--- MoooLien/MoooLien/Models/Entities/Assignment.cs
+++ MoooLien/MoooLien/Models/Entities/Assignment.cs
@@ -8,7 +8,7 @@ namespace MoooLien.Models.Entities
     public class Assignment
     {
         public int ID { get; set; }
-        public int courseID { get; set; }
+        public string courseID { get; set; }
         public string name { get; set; }
         public string solution { get; set;}
         public int handinCounter { get; set; }
