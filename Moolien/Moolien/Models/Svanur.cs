143a48cf4e4ed3411d0b490aacea333a093ad965
e-magnus <einare15@ru.is>
2016-05-06 11:36:28 +0000

    Courses html

---
+++ Moolien/Moolien/Models/Svanur.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Moolien.Models
+{
+    public class Svanur
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
