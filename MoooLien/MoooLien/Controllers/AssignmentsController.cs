16406b87f7f010744c972b04f0a31d31c9d7e3e8
e-magnus <einare15@ru.is>
2016-05-06 15:20:11 +0000

    Kaffitími

--- MoooLien/MoooLien/Controllers/AssignmentsController.cs
+++ MoooLien/MoooLien/Controllers/AssignmentsController.cs
@@ -47,7 +47,7 @@ namespace MoooLien.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,courseID,name")] Assignment assignment)
+        public ActionResult Create([Bind(Include = "ID,courseID,name,solution")] Assignment assignment)
         {
             if (ModelState.IsValid)
             {
@@ -79,7 +79,7 @@ namespace MoooLien.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,courseID,name")] Assignment assignment)
+        public ActionResult Edit([Bind(Include = "ID,courseID,name,solution")] Assignment assignment)
         {
             if (ModelState.IsValid)
             {
