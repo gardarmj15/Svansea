1fe80a21c36ca603d4918eea60935dee0a4764e5
e-magnus <einare15@ru.is>
2016-05-08 17:13:08 +0000

    Fallergri user tafla

--- MoooLien/MoooLien/Controllers/AccountController.cs
+++ MoooLien/MoooLien/Controllers/AccountController.cs
@@ -136,7 +136,7 @@ namespace MoooLien.Controllers
 
         //
         // GET: /Account/Register
-        [AllowAnonymous]
+        [Authorize]
         public ActionResult Register()
         {
             return View();
@@ -145,7 +145,7 @@ namespace MoooLien.Controllers
         //
         // POST: /Account/Register
         [HttpPost]
-        [AllowAnonymous]
+        [Authorize]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Register(RegisterViewModel model)
         {
