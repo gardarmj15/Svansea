bf259a7657c651d9d0dcdc51b1ae0cd1fd489a69
Svanurjoh <svanurjoh15@ru.is>
2016-05-09 21:06:04 +0000

    database stuff

---
+++ MoooLien/MoooLien/Models/Entities/Roles.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MoooLien.Models.Entities
+{
+    public class Roles
+    {
+        public int roleID { get; set; }
+        public string name { get; set; }
+    }
+}
---
+++ MoooLien/MoooLien/Models/Entities/UsersInCourse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MoooLien.Models.Entities
+{
+    public class UsersInCourse
+    {
+        public string userID { get; set; }
+        public int courseID { get; set; }
+        public int roleID { get; set; }
+    }
+}
