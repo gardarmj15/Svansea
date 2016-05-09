07fe1fa3d72021fced5e5bb2be93c4da9743199a
e-magnus <einare15@ru.is>
2016-05-09 21:05:27 +0000

    Layourt breytingar

---
+++ MoooLien/MoooLien/Models/ApplicationRole.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+public class ApplicationRole : IdentityRole
+{
+    public ApplicationRole() : base() { }
+    public ApplicationRole(string name) : base(name) { }
+    public string Description { get; set; }
+
+}
