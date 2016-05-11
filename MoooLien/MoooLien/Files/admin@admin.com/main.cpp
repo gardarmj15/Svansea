c1cc9761e4ec73e3831a30cbe809bbfbfa9a4bf2
e-magnus <einare15@ru.is>
2016-05-11 22:49:18 +0000

    Compiler þýðir main.cpp í notendamöppu

---
+++ MoooLien/MoooLien/Files/admin@admin.com/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+cout << "Hello world" << endl;
+cout << "The output should contain two lines" << endl;
+cout << "The output should contain three lines" << endl;
+return 0;
+}
