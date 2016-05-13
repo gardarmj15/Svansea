480f7d73fc0da2beab3d7415c86cfaf4dd615196
e-magnus <einare15@ru.is>
2016-05-13 07:40:58 +0000

    123

--- MoooLien/MoooLien/Files/Magnus/main.cpp
+++ MoooLien/MoooLien/Files/Magnus/main.cpp
@@ -5,6 +5,7 @@ int main()
 cout << "This is line number one." << endl;
 cout << "This is line number two." << endl;
 cout << "This is line number three." << endl;
+cout << "This is line number four." << endl;
 
 return 0;
 }
